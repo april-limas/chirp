07e740ab9f7784adc561c1eb74c14a1a9dcfe47a
April Limas <aprilj.limas@gmail.com>
2020-09-08 23:55:25 -0400

    added dispatch getMessageList action to update message list with new message automatically

--- src/components/add-message/Add-Message.js
+++ src/components/add-message/Add-Message.js
@@ -6,23 +6,25 @@ import { Loader } from "../loader"
 export const AddMessage = () => {
     const [text, setText ] = useState("")
     const messageLoading = useSelector(state => state.message.messageLoading)
+
     const dispatch = useDispatch()
 
-    const handleOnChange = (e) => (
+    const handleChange = (e) => (
             setText(e.target.value)
     )
 
-    const handlePostMessage = (event) => {
+    const handleAddMessage = (event) => {
         event.preventDefault()
         dispatch(actions.postMessage(text))
+        dispatch(actions.getMessageList())
         setText("")
     }
 
     return (
         <>
-            <form onSubmit={handlePostMessage}>
+            <form onSubmit={handleAddMessage}>
                 <p>What's on your mind?</p>
-                <input type="text" value={text} onChange={handleOnChange} placeholder="New Message"></input>
+                <input type="text" value={text} onChange={handleChange} placeholder="New Message"></input>
                 <button type="submit">Post New Message</button>
             </form>
             { messageLoading && <Loader /> }
