05acba77ff924394fb224508806ef5827b734216
Kamela Williamson <kami63116@yahoo.com>
2020-09-09 18:35:35 -0700

    finished followers component

--- src/components/followers/Followers.js
+++ src/components/followers/Followers.js
@@ -1,36 +1,27 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { actions } from '../../redux/actions/users';
-
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { actions } from "../../redux/actions/users";
+import { FollowersItem } from "../followers-item";
+import {v4} from "uuid";
 
 export const Followers = () => {
-  const { data } = useSelector(state => ({
-    data: state.users.usersList.users
-}
-));
+  const { data } = useSelector((state) => ({
+    data: state.users.usersList.users,
+  }));
 
-        console.log(data)
-        const dispatch = useDispatch()
+  console.log({data});
+  const dispatch = useDispatch();
 
-        useEffect(()=> {
-          dispatch(actions.getFollowers())
-        },[]) 
+  useEffect(() => {
+    dispatch(actions.getFollowers());
+  }, []);
 
-        
-      
-        return (
-        <>
-            <h2>Followers</h2>
+  return (
+    <>
+      <h2>Followers</h2>
 
-            <ul>
-                {data && data.map(item => (
-                <li>{item.displayName}</li>
-                ))}
-            </ul>
-            
-      
-           
-          </>
-        )
-      }
+      <ul>{data && data.map((item) => <FollowersItem user={item} key={v4()}/>)}</ul>
+    </>
+  );
+};
