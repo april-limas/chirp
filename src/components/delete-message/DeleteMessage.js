8b0ea252912aeb751dc293fcd437585e60643cc7
DeQuan Mitchell <dequanmitchell83@gmail.com>
2020-09-10 14:17:30 -0400

    DeleteMessage component functionality works as desired

--- src/components/delete-message/DeleteMessage.js
+++ src/components/delete-message/DeleteMessage.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { actions } from '../../redux/actions/messages';
 
-export const DeleteMessage = () => {
+export const DeleteMessage = ({message}) => {
+
+    const [isCurrentUser, seIsCurrentUser] = useState()
+    const user = useSelector(state => state.auth.username)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (message.message.username === user) {
+            seIsCurrentUser(true)
+        }
+    })
     
+    const handleDeleteMessage = () => {
+        dispatch(actions.deleteMessage(message.message.id))
+    }
+
+    return (
+        <>
+        {isCurrentUser ? 
+        <button onClick={handleDeleteMessage}>Delete Message</button>
+        : null}
+        </>
+    )
 }
