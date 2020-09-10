4d5a63df6a97f1034883b99bedb08f6d73db5887
April Limas <aprilj.limas@gmail.com>
2020-09-09 20:16:09 -0400

    formatted rendered data

--- src/components/message-item/MessageItem.js
+++ src/components/message-item/MessageItem.js
@@ -17,7 +17,6 @@ export const MessageItem = ({item}) => {
     dispatch(messageActions.getMessageLinkInfo(item.id))
     
   }
-
   
     return (
       <>
--- src/components/message-link-info/MessageLinkInfo.js
+++ src/components/message-link-info/MessageLinkInfo.js
@@ -3,13 +3,16 @@ import { useSelector } from 'react-redux';
 
 
 export const MessageLinkInfo = () => {
-    const {message}=useSelector(state=>state.message.message)
-    console.log({message})
+    const {message} = useSelector(state => state.message.message)
+    
     return (
         <>
+        <br />
+        <h2>Tweet Info</h2>
+        <br />
         <p>User: {message.username}</p>
         <p>Tweet: {message.text}</p>
-        <p>Date posted: {message.createdAt}</p>
+        <p>Date Posted: {message.createdAt}</p>
         <p>Likes: {message.likes.length}</p>
         </>
     )
